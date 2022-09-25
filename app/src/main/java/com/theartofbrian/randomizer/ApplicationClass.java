bb29200a03a1206d26fdd7bb05128a526c6dbdd6
Brian Estremera <themechanist@gmail.com>
2022-09-24 20:26:07 -0400

    Add OneSignal Gradle Plugin and SDK

---
+++ app/src/main/java/com/theartofbrian/randomizer/ApplicationClass.java
@@ -0,0 +1,11 @@
+package com.theartofbrian.randomizer;
+
+import android.app.Application;
+
+public class ApplicationClass extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: Add OneSignal initialization here
+    }
+}
